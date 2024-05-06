1a76f3bf763ad5841da65f86240c74a04445ed32
douglasdl <douglas_san@hotmail.com>
2024-05-06 16:49:53 -0300

    style: category list padding layout

--- foods/app/_components/category-list.tsx
+++ foods/app/_components/category-list.tsx
@@ -4,7 +4,7 @@ import { CategoryItem } from "./category-item";
 export async function CategoryList() {
   const categories = await db.category.findMany({});
   return (
-    <div className="flex gap-3 overflow-x-scroll">
+    <div className="flex gap-3 overflow-x-scroll px-5">
       {categories.map((category) => (
         <CategoryItem key={category.id} category={category} />
       ))}
