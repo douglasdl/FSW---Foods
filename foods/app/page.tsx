c966fd6b7bdad10964db76aafd6b101f82947b32
douglasdl <douglas_san@hotmail.com>
2024-05-06 14:53:05 -0300

    feat: add promo banners

--- foods/app/page.tsx
+++ foods/app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { CategoryList } from "./_components/category-list";
 import { Header } from "./_components/header";
 import { Search } from "./_components/search";
@@ -11,6 +12,29 @@ export default function Home() {
       </div>
       <div className="px-5 pt-6">
         <CategoryList />
+      </div>
+      <div className="px-5 pt-6">
+        <Image
+          src="/banner-pizza.png"
+          alt="Até 30% de desconto em pizzas"
+          width={0}
+          height={0}
+          className="h-auto w-full object-contain"
+          sizes="100vw"
+          quality={100}
+        />
+      </div>
+
+      <div className="px-5 pt-6">
+        <Image
+          src="/banner-burguer.png"
+          alt="A partir de R$17,90 em lanches"
+          width={0}
+          height={0}
+          className="h-auto w-full object-contain"
+          sizes="100vw"
+          quality={100}
+        />
       </div>
     </div>
   );
